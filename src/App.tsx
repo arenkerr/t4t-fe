f9caa7af793371ea7ce52a2e9e340186171186bd
Aren <ani.kerr@gmail.com>
2023-04-15 17:26:36 -0400

    add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import logo from './logo.svg';
 import './App.css';
 
+const GET_TEST_DATA = gql`
+  query GetBooks {
+    books {
+      author
+      title
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(GET_TEST_DATA);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
