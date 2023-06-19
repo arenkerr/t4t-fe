ce40956dd347e413f7537179c14533beaaed7d2d
Aren <ani.kerr@gmail.com>
2023-06-19 18:29:11 -0400

    set up storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from '@mui/material';
-import LogIn from './pages/logIn/logIn';
-import SignUp from './pages/signUp/signUp';
+import LogIn from './pages/logIn/logIn.page';
+import SignUp from './pages/signUp/signUp.page';
 
 const App = () => {
   return (
---
+++ src/components/button/button.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './button.component';
+
+const meta: Meta<typeof Button> = {
+  title: 'Stories/Components/Button',
+  component: Button,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const Primary: Story = {
+  // More on args: https://storybook.js.org/docs/react/writing-stories/args
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Button',
+  },
+};
