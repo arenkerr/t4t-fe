1255374c24c821d6e89d5eebaed070dff2eacf64
Aren <ani.kerr@gmail.com>
2023-06-24 19:39:13 -0400

    add button component

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -5,28 +5,21 @@ import { Button } from './button.component';
 const meta: Meta<typeof Button> = {
   title: 'Stories/Components/Button',
   component: Button,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-export const Primary: Story = {
-  // More on args: https://storybook.js.org/docs/react/writing-stories/args
+export const Contained: Story = {
   args: {
-    primary: true,
     label: 'Button',
   },
 };
 
-export const Secondary: Story = {
+export const Outlined: Story = {
   args: {
+    variant: 'outlined',
     label: 'Button',
   },
 };
--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import '@fontsource/nunito/300.css';
 import '@fontsource/nunito/400.css';
 import '@fontsource/nunito/500.css';
 import '@fontsource/nunito/700.css';
+import '@fontsource/shrikhand/400.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
